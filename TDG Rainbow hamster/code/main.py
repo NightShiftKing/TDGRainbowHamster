68f511a57925a3d6b1089ce525b9db5175bf692d
NightShiftKing <michaelmonreal482@gmail.com>
2025-05-19 22:06:46 -0600

    made player, map, entity class. cleaned up main.py, need to fix animation

--- TDG Rainbow hamster/code/main.py
+++ TDG Rainbow hamster/code/main.py
@@ -5,121 +5,41 @@ from player import Player
 pygame.init()
 pygame.display.set_caption("sprite sheet")  # sets the window title
 screen = pygame.display.set_mode((800,800))  # creates game screen
-screen.fill((0,0,0))
+
 clock = pygame.time.Clock() #set up clock
 gameover = False #variable to run our game loop
 
-# CONSTANTS
-LEFT = 0
-RIGHT = 1
-UP = 2
-DOWN = 3
-SPACE = 4
-keys = [False, False, False, False, False] #this list holds whether each key has been pressed
-mapNum = 1
-
 #maps
-
-map = Map.MAP1.value
-
-twomap = Map.MAP2.value
-
-
-wall = pygame.image.load('stone.png')  # load your spritesheet
-
-Rock = pygame.image.load('blocker.png') # load your spritesheet
-Puddle = pygame.image.load('potion thing.png') # load your spritesheet
-Portal = pygame.image.load('DOOR.png')
-
+map = Map()
 
 # player variables
 xpos = 400  # xpos of player
 ypos = 400  # ypos of player
+PunkHamster = Player(xpos,ypos,'TDG Rainbow hamster\\images\\The PUNK.png')
 
-PunkHamster = Player(xpos,ypos,'The Punk.png')
 
-ticker = 0
-direction = DOWN
-
+#game loop
 while not gameover:
+    ticks = clock.get_time()
     clock.tick(60)  # FPS
+    gameEvents = pygame.event.get()
 
-    for event in pygame.event.get():  # quit game if x is pressed in top corner
+    for event in gameEvents:  # quit game if x is pressed in top corner
         if event.type == pygame.QUIT:
             gameover = True
 
-        #keyboard input---------------------------
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                keys[LEFT] = True
-            elif event.key == pygame.K_RIGHT:
-                keys[RIGHT] = True
-            elif event.key == pygame.K_UP:
-                keys[UP] = True
-            elif event.key == pygame.K_DOWN:
-                keys[DOWN] = True
-
-
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                keys[LEFT] = False
-            elif event.key == pygame.K_RIGHT:
-                keys[RIGHT] = False
-            elif event.key == pygame.K_UP:
-                keys[UP] = False
-            elif event.key == pygame.K_DOWN:
-                keys[DOWN] = False
-
-
-
-
-    #Left/right MOVEMENT-------------------------------------
+    #keyboard input---------------------------
     PunkHamster.move() 
-
     #map collision---------------------
-
-
-
-
-
-
+    map.map_collision(PunkHamster)
     # Animation update
-    ticker+=1
-    if vx != 0: #only animate when moving
-        if ticker % 10 == 0:  # only change frames every 10 ticks (make number smaller for faster running animation)
-            frameNum += 1
-    if frameNum > 1:
-        frameNum = 0
-
+    PunkHamster.animate()
     # Render section--------------------------------------------------------
     screen.fill((0, 0, 0))  # wipe screen so it doesn't smear
-    # draw map
-    if mapNum == 1:
-        for i in range(20):
-            for j in range(20):
-                if map[i][j] == 2:
-                    screen.blit(wall, (j * 40, i * 40), (0, 0, 40, 40))
-                elif map[i][j] == 3:
-                    screen.blit(Rock, (j * 40, i * 40), (0, 0, 40, 40))
-                elif map[i][j] == 4:
-                    screen.blit(Puddle, (j * 40, i * 40), (0, 0, 40, 40))
-                elif map[i][j] == 5:
-                    screen.blit(Portal, (j * 40, i * 40), (0, 0, 40, 40))
-
-    if mapNum == 2:
-        for i in range(20):
-            for j in range(20):
-                if twomap[i][j] == 2:
-                    screen.blit(wall, (j * 40, i * 40), (0, 0, 40, 40))
-                elif twomap[i][j] == 3:
-                    screen.blit(Rock, (j * 40, i * 40), (0, 0, 40, 40))
-                elif twomap[i][j] == 4:
-                    screen.blit(Puddle, (j * 40, i * 40), (0, 0, 40, 40))
-                elif twomap[i][j] == 5:
-                    screen.blit(Portal, (j * 40, i * 40), (0, 0, 40, 40))
-
-    # draw player
     PunkHamster.draw(screen)
+    # draw map
+    map.drawMap(screen)
+    # draw player
     pygame.display.flip()  # this actually puts the pixel on the screen
 
 # end game loop
--- TDG Rainbow hamster/code/player.py
+++ TDG Rainbow hamster/code/player.py
@@ -2,18 +2,19 @@ import pygame
 from entity import Entity
 
 class Player(Entity):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,xpos, ypos, image_path):
+        super().__init__(xpos, ypos, image_path)
 
         #animation variables
         self.frameWidth = 60
         self.frameHeight = 60
         self.RowNum = 0  # for left animation, this will need to change for other animations
         self.frameNum = 0
+        self.ticker = 0 
         self.image.set_colorkey((255,0,255))
 
     def draw(self, screen):
-        screen.blit(self.image, (self.xpos, self.ypos), (self.frameWidth * self.frameNum, self.RowNum * self.frameHeight, self.frameWidth, self.frameHeight))
+        screen.blit(self.image, (self.pos.x, self.pos.y), (self.frameWidth * self.frameNum, self.RowNum * self.frameHeight, self.frameWidth, self.frameHeight))
 
     def move(self, speed = 10):
         keys = pygame.key.get_pressed()
@@ -22,8 +23,14 @@ class Player(Entity):
 
         self.pos += self.velocity
 
-    def animate():
-        -
+
+    def animate(self):
+        self.ticker+=1
+        if self.velocity.x != 0: #only animate when moving
+            if self.ticker % 10 == 0:  # only change frames every 10 ticks (make number smaller for faster running animation)
+                self.frameNum += 1
+        if self.frameNum > 7:
+            self.frameNum = 0
 
 
          
